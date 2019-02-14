e8ae3b50c21c6c4566f30387f0ceeb3674fee831
liujunsheng <401865981@qq.com>
2019-02-14 19:00:32 +0800

    N皇后

--- lintcode/n-queens.py
+++ lintcode/n-queens.py
@@ -2,6 +2,77 @@
 # -*- coding: utf-8 -*-
 
 """
+https://www.lintcode.com/problem/n-queens/description
+描述
+中文
+English
+n皇后问题是将n个皇后放置在n*n的棋盘上，皇后彼此之间不能相互攻击。
+给定一个整数n，返回所有不同的n皇后问题的解决方案。
+每个解决方案包含一个明确的n皇后放置布局，其中“Q”和“.”分别表示一个女王和一个空位置。
+
+输入:1
+输出:
+   [["Q"]]
+
+输入:4
+输出:
+[
+  [".Q..",
+   "...Q",
+   "Q...",
+   "..Q."
+  ],
+  ["..Q.",
+   "Q...",
+   "...Q",
+   ".Q.."
+  ]
+]
+"""
+
+
+class Solution1:
+    """
+    @param: n: The number of queens
+    @return: All distinct solutions
+    """
+    def solveNQueens(self, n):
+        if n < 1:
+            return []
+        ans = []
+        tmp = ['.'] * n
+        for queen in self.dfs(n, [0] * n, 0):
+            addr = []
+            for j in queen:
+                tmp[j] = 'Q'
+                addr.append(''.join(tmp))
+                tmp[j] = '.'
+            # end for
+            ans.append(addr)
+        # end for
+        return ans
+
+    def is_ok(self, queens, idx, v):
+        for i, j in enumerate(queens[: idx]):
+            if j == v or abs(j - v) == idx - i:
+                return False
+        return True
+
+    def dfs(self, n, queens, idx):
+        if idx >= n:
+            return [list(queens)]
+        ans = []
+        for i in range(n):
+            if self.is_ok(queens, idx, i):
+                queens[idx] = i
+                ans.extend(self.dfs(n, queens, idx + 1))
+            # end if
+        # end for
+        return ans
+
+print(Solution1().solveNQueens(4))
+
+"""
 https://www.lintcode.com/problem/n-queens-ii/description
 根据n皇后问题，现在返回n皇后不同的解决方案的数量而不是具体的放置布局
 example:
@@ -21,7 +92,7 @@ example:
 """
 
 
-class Solution:
+class Solution2:
 
     """
     @param n: The number of queens.
@@ -50,4 +121,4 @@ class Solution:
         # end for
         return ans
 
-print(Solution().totalNQueens(15))
+print(Solution2().totalNQueens(4))
