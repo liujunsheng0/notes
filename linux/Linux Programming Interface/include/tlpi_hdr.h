565d08d3888b33cd30911b80f4639d07a180f6da
liujunsheng <401865981@qq.com>
2019-03-01 21:52:01 +0800

    系统编程手册

---
+++ linux/Linux Programming Interface/include/tlpi_hdr.h
@@ -0,0 +1,20 @@
+//
+// Created by Administrator on 2019/3/1.
+// 学习过程中用到的头文件
+//
+
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>  // 系统调用原型方法
+#include <errno.h>   //
+#include <string.h>
+
+typedef enum {FALSE, TRUE} Boolean;
+#define min(x, y) ((m) < (n) ? (m): (n))
+#define min(x, y) ((m) < (n) ? (n): (m))
+
+#endif  // #ifndef TLPI_HDR_H
