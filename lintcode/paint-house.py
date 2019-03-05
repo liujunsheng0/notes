5f86b54dd4b0973a6f0eb4845059b408020c7f35
liujunsheng <401865981@qq.com>
2019-03-05 21:56:08 +0800

    房屋染色

---
+++ lintcode/paint-house.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+房屋染色  https://www.lintcode.com/problem/paint-house/description
+
+这里有n个房子在一列直线上，现在我们需要给房屋染色，分别有红色蓝色和绿色。每个房屋染不同的颜色费用也不同，你需要设计一种染色方
+案使得相邻的房屋颜色不同，并且费用最小，返回最小的费用。费用通过一个nx3 的矩阵给出，
+比如cost[0][0]表示房屋0染红色的费用，cost[1][2]表示房屋1染绿色的费用。所有费用都是正整数
+
+输入: [[14,2,11],[11,14,5],[14,3,10]]  输出: 10
+解释: 蓝 绿 蓝, 2 + 5 + 3 = 10
+输入: [[1,2,3],[1,4,6]]
+输出: 3
+"""
+
+"""
+房屋染色 II https://www.lintcode.com/problem/paint-house-ii/description
+这里有n个房子在一列直线上，现在我们需要给房屋染色，共有k种颜色。每个房屋染不同的颜色费用也不同，你需要设计一种染色方案使得相邻的房屋颜色不同，并且费用最小。
+费用通过一个nxk 的矩阵给出，比如cost[0][0]表示房屋0染颜色0的费用，cost[1][2]表示房屋1染颜色2的费用。
+所有费用都是正整数
+costs = [[14,2,11],[11,14,5],[14,3,10]] return 10
+房屋 0 颜色 1, 房屋 1 颜色 2, 房屋 2 颜色 1， 2 + 5 + 3 = 10
+挑战
+用O(nk)的时间复杂度解决
+"""
+
+
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+
+    def minCost(self, costs):
+        if len(costs) < 1:
+            return 0
+        costs = [list(i) for i in costs]
+        for idx, v in enumerate(costs[1:]):
+            v[0] += min(costs[idx][1], costs[idx][2])
+            v[1] += min(costs[idx][0], costs[idx][2])
+            v[2] += min(costs[idx][0], costs[idx][1])
+        # end for
+        return min(costs[-1])
+
+    """
+    @param costs: n x k cost matrix
+    @return: an integer, the minimum cost to paint all houses
+    """
+
+    def minCostII(self, costs):
+        if len(costs) < 1:
+            return 0
+        for idx, cost in enumerate(costs[1:]):
+            for idy in range(len(cost)):
+                cost[idy] += min(costs[idx][:idy] + costs[idx][idy + 1:])
+        # end for
+        return min(costs[-1])
+
+
+print(Solution().minCostII([[1, 2, 3], [1, 4, 6]]))
