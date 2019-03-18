79a9ee1d9d4939ea1229f3e7a67f6fa683698945
liujunsheng <401865981@qq.com>
2019-03-18 13:31:16 +0800

    单词切分2

--- lintcode/word-break.py
+++ lintcode/word-break.py
@@ -18,7 +18,7 @@ class Solution:
     @return: A boolean
     """
     def wordBreak(self, s, dicts):
-        """ DFS 内存溢出 """
+        """ DFS 内存超出限制 """
         if not s:
             return True
         for i in dicts:
@@ -46,3 +46,43 @@ class Solution:
 
 print(Solution().wordBreak("abcdadada", ["a", "d", "c", "b"]))
 print(Solution().wordBreak2("abcdadada", ["a", "d", "c", "b"]))
+
+
+"""
+描述 https://www.lintcode.com/problem/word-break-ii/description
+给一字串s和单词的字典dict,在字串中增加空格来构建一个句子，并且所有单词都来自字典。返回所有有可能的句子。
+给一字串lintcode,字典为["de", "ding", "co", "code", "lint"] 则结果为["lint code", "lint co de"]
+"""
+
+# TODO: 未AC
+class Solution:
+    """
+    @param: s: A string
+    @param: wordDict: A set of words.
+    @return: All possible sentences.
+    """
+    def wordBreak(self, s, wordDict):
+        """内存超出限制"""
+        if not s:
+            return True
+        size = len(s)
+        # dp[i] == True 表示 s中0~i是可以由dicts中的单词组成的
+        dp = [[False, []] for _ in range(size + 1)]
+        dp[0][0] = True
+        for idx, (v, path) in enumerate(dp):
+            if not v:
+                continue
+            for w in wordDict:
+                tmp = len(w)
+                if w == s[idx: idx + tmp]:
+                    dp[idx + tmp][0] = True
+                    dp[idx + tmp][1] += [i + [w] for i in path] if path else [[w]]
+                # end if
+            # end for
+        # end for
+        return [" ".join(i) for i in dp[-1][-1]]
+
+print(Solution().wordBreak("abcdadada", ["a", "d", "c", "b"]))
+print(Solution().wordBreak("lintcode", ["de","ding","co","code","lint"]))
+print(Solution().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                           ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
