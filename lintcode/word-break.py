40c473b7644ecc6bf5f51d66600e71406876395a
liujunsheng <liujunsheng0@qq.com>
2019-12-26 15:27:15 +0800

    [Lintcode] word-break-iii

--- lintcode/word-break.py
+++ lintcode/word-break.py
@@ -86,3 +86,68 @@ print(Solution().wordBreak("abcdadada", ["a", "d", "c", "b"]))
 print(Solution().wordBreak("lintcode", ["de","ding","co","code","lint"]))
 print(Solution().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
                            ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
+
+
+
+"""
+https://www.lintcode.com/problem/word-break-iii/description?_from=ladder&&fromId=137
+给出一个单词表和一条去掉所有空格的句子，根据给出的单词表添加空格, 返回可以构成的句子的数量, 保证构成的句子中所有的单词都可以在单词表中找到.
+忽略大小写
+
+输入：
+"CatMat"
+["Cat", "Mat", "Ca", "tM", "at", "C", "Dog", "og", "Do"]
+输出： 3
+解释：
+我们可以有如下三种方式：
+"CatMat" = "Cat" + "Mat"
+"CatMat" = "Ca" + "tM" + "at"
+"CatMat" = "C" + "at" + "Mat"
+"""
+
+
+class Solution:
+    """
+    @param: : A string
+    @param: : A set of word
+    @return: the number of possible sentences.
+    """
+
+    def wordBreak3(self, s: str, words: list):
+        s = s.lower()
+        words = set([i.lower() for i in words])
+        # return self.recursion(s, words)
+        return self.dp(s, words)
+
+    # 枚举, Time Limit Exceeded
+    def recursion(self, s: str, words):
+        if not s:
+            return 1
+        ans = 0
+        for w in words:
+            if s.startswith(w):
+                ans += self.recursion(s[len(w):], words)
+        return ans
+
+    # 动态规划
+    def dp(self, s: str, words: set):
+        size = len(s)
+        dp = [0] * (size + 1)
+        dp[0] = 1
+        for i in range(1, size + 1):
+            for w in words:
+                w_size = len(w)
+                if i - w_size >= 0 and s[i - w_size: i] == w:
+                    # 子串的拼法组合而成
+                    dp[i] += dp[i - w_size]
+        return dp[-1]
+
+
+#
+words = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+# 84675106
+# 265816832
+print(Solution().wordBreak3("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", words) == 265816832)
+
+words = ["Cat", "mat", "Ca", "tm", "at", "C", "Dog", "og", "Do"]
+print(Solution().wordBreak3("Catmat", words))
